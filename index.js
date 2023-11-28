3ccbfd6eef310c167952ae09138022cc059221a6
Shasew <Shadowgaming126000@gmail.com>
2023-11-28 14:08:55 +0700

    ok

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
-console.log("fernando")
+const express = require ("express");
+const path = require("path");
+const app = express();
+
+app.use(express.json());
+
+app.get("/" , (req, res) => {
+    res.sendFile(path.join(__dirname, "index.html"));
+});
+
+const port = 3000
+app.listen(port, "0.0.0.0", () => {
+    console.log(`Example app Listening at http://kalix)
+localhost:${port}`);
+});
